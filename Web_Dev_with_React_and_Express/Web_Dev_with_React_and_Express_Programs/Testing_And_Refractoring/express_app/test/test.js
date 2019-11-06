9f29d067cd0a4123b9ff4f13d4b56ec1d1f9d679
LiamCJ <liamcjones2000@gmail.com>
2019-11-06 12:34:03 +0200

    Web_Dev_with_React_and_Express

--- Web_Dev_with_React_and_Express/Web_Dev_with_React_and_Express_Programs/Testing_And_Refractoring/express_app/test/test.js
+++
@@ -1,9 +0,0 @@
-const assert = require("chai").assert;
-const index = require("../index.js")
-
-//testing the function
-describe("tax calculate" , () => {
-	it("Checking for functionality", () => {
-		assert.equal(index(450000),262263)
-	})
-})
